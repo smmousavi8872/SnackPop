88b6fcb6da06b0510217b207ab2762c1b096d0be
mo.mousavi <s.m.mousavi1993@gmail.com>
2024-03-06 15:43:06 +0330

    imepl: raw source added

---
+++ app/src/main/java/com/github/smmousavi/snackpop/snackpop/SnackDuration.kt
@@ -0,0 +1,5 @@
+package com.github.smmousavi.snackpop.snackpop
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.SOURCE)
+annotation class SnackDuration
---
+++ app/src/main/java/com/github/smmousavi/snackpop/snackpop/SnackType.kt
@@ -0,0 +1,10 @@
+package com.github.smmousavi.snackpop.snackpop
+
+@Target(
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.CLASS
+)
+@Retention(AnnotationRetention.BINARY)
+annotation class SnackType
